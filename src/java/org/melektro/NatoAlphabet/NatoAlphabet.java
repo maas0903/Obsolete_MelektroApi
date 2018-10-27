70c5a1ff26ae6b7225e504ca88f796ece6b0ee38
maas0903 <marius.tennapel@hotmail.com>
2018-10-27 13:51:22 +0200

    123

--- src/java/org/melektro/NatoAlphabet/NatoAlphabet.java
+++ src/java/org/melektro/NatoAlphabet/NatoAlphabet.java
@@ -5,9 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
 import static com.melektro.Tools.Nato.GetNato;
 
 /**
@@ -16,10 +14,6 @@ import static com.melektro.Tools.Nato.GetNato;
  */
 @Path("nato")
 public class NatoAlphabet {
-
-    @Context
-    private UriInfo context;
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("getnato/")
